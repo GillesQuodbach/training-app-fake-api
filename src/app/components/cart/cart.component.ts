b9fa031bf0d739061cef6b1fb986707d64a365a1
Gilles <quodbach.gilles@gmail.com>
2024-03-05 13:04:41 +0100

    ajout pipe ok

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -6,30 +6,31 @@ import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 
 /**
  * Composant de gestion d'un panier permettant l'affichage du panier, la suppression éventuelle de formations et le passage à l'étape suivante
  */
 export class CartComponent implements OnInit {
-  cart : Training[] | undefined;
-  amount : number = 0;
-  constructor(private cartService : CartService , private router : Router) { }
+  cart: Training[] | undefined;
+  amount: number = 0;
+  dateOrder: Date = new Date();
+  constructor(public cartService: CartService, private router: Router) {}
 
   /**
    * à l'initialisation du composant, récupération des données du panier via le service dédié
    */
   ngOnInit(): void {
-    this.cart = this.cartService.getCart();       
+    this.cart = this.cartService.getCart();
     this.amount = this.cartService.getAmount();
   }
 
   /**
    * Méthode qui supprime une formation du panier et met à jour l'affichage du panier
-   * @param training 
+   * @param training
    */
-  onRemoveFromCart(training : Training){
+  onRemoveFromCart(training: Training) {
     this.cartService.removeTraining(training);
     this.cart = this.cartService.getCart();
   }
@@ -37,7 +38,7 @@ export class CartComponent implements OnInit {
   /**
    * Méthode de gestion de l'étape suivante de la commande en renvoyant vers le composant de gestion client (formulaire)
    */
-  onNewOrder(){
+  onNewOrder() {
     this.router.navigateByUrl('customer');
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,52 +3,53 @@ import { Customer } from '../model/customer.model';
 import { Training } from '../model/training.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 /**
  * Service dédié à la gestion du panier à l'aide du local storage qui contient à tout instant les éléments d'un panier
- * jusqu'à la validation de celui-ci qui provoquera la suppresion dans le LS du panier à l'exception du customer 
+ * jusqu'à la validation de celui-ci qui provoquera la suppresion dans le LS du panier à l'exception du customer
  */
-export class CartService {  
-  private cart : Map<number,Training>;  // panier
+export class CartService {
+  private cart: Map<number, Training>; // panier
 
-  constructor() {     
+  constructor() {
     // au démarrage du service, je récupère le contenu du local storage : commande en cours
     let cart = localStorage.getItem('cart');
-    if(cart){  // le panier existe déjà
+    if (cart) {
+      // le panier existe déjà
       this.cart = new Map(JSON.parse(cart));
     } // sinon il faut le créer
-    else this.cart = new Map<number,Training>();
+    else this.cart = new Map<number, Training>();
   }
 
   /**
    * Méthode qui ajoute une formation au panier puis ajoute le panier au local storage
-   * @param training 
+   * @param training
    */
-  addTraining(training: Training) { 
-    this.cart.set(training.id,training);
+  addTraining(training: Training) {
+    this.cart.set(training.id, training);
     this.saveCart(); //à chaque fois que j'ajoute un élément au panier, je met à jour le local storage
   }
 
   /**
    * Méthode qui ajoute un client au Local storage, s'il existe déjà il est écrasé
-   * @param customer 
+   * @param customer
    */
-  saveCustomer(customer : Customer) {
-    localStorage.setItem('customer',JSON.stringify(customer));
+  saveCustomer(customer: Customer) {
+    localStorage.setItem('customer', JSON.stringify(customer));
   }
 
   /**
    * Méthode qui injecte le contenu du panier dans le local storage
    */
   saveCart() {
-    localStorage.setItem('cart',JSON.stringify([...this.cart]));
+    localStorage.setItem('cart', JSON.stringify([...this.cart]));
   }
 
   /**
    * Méthode qui retire une formation au panier puis met à jour le LS
-   * @param training 
+   * @param training
    */
   removeTraining(training: Training) {
     this.cart.delete(training.id);
@@ -59,9 +60,8 @@ export class CartService {
    * Méthode qui renvoi le contenu du panier sous forme de tableau
    * @returns Training [] | undefined
    */
-  getCart() : Training [] | undefined {    
-    if(this.cart.size > 0)
-    return Array.from(this.cart.values());
+  getCart(): Training[] | undefined {
+    if (this.cart.size > 0) return Array.from(this.cart.values());
     else return undefined;
   }
 
@@ -69,22 +69,22 @@ export class CartService {
    * Méthode qui calcule et renvoi le montant total du panier
    * @return total amount
    */
-  getAmount() : number {
-    let amount : number = 0;
-    this.cart.forEach(training => {
+  getAmount(): number {
+    let amount: number = 0;
+    this.cart.forEach((training) => {
       amount += training.price * training.quantity;
     });
-    return amount;    
+    return amount;
   }
 
   /**
    * Méthode qui renvoi le client à partir du LS s'il existe sinon une instance la classe Customer
-   * @return 
+   * @return
    */
-  getCustomer() : Customer {
+  getCustomer(): Customer {
     let customer = localStorage.getItem('customer');
-    if(customer)  return  JSON.parse(customer);
-    return new Customer("unknown","","","","");
+    if (customer) return JSON.parse(customer);
+    return new Customer('unknown', '', '', '', '');
   }
 
   /**
@@ -92,6 +92,6 @@ export class CartService {
    */
   clearLocalStorage() {
     this.cart.clear();
-    localStorage.setItem('cart','');    
+    localStorage.setItem('cart', '');
   }
 }
