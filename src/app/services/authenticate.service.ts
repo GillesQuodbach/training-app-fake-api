ae19795e0a3a585f933c077ed2cbad85aacb7eb1
Gilles <quodbach.gilles@gmail.com>
2024-03-05 15:03:21 +0100

    ajout model user + login form

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -6,8 +7,22 @@ import { Injectable } from '@angular/core';
 export class AuthenticateService {
   constructor() {}
 
+  private users: User[] = [
+    {
+      email: 'elbab@gmail.com',
+      password: '1234',
+      roles: ['ADMIN_USER'],
+    },
+    {
+      email: 'hugo@gmail.com',
+      password: '1234',
+      roles: ['USER'],
+    },
+  ];
+
   isAdmin() {
+    // TODO Récupérer les champs email et password du login
     console.log('admin is read');
-    return true;
+    return false;
   }
 }
