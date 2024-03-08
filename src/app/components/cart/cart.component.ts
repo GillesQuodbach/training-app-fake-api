b405d4655171faa6e641f3d47fd53a5adec463a5
Gilles <quodbach.gilles@gmail.com>
2024-03-08 14:20:00 +0100

    maj form

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Training } from 'src/app/model/training.model';
 import { CartService } from 'src/app/services/cart.service';
+import { AuthenticateService } from 'src/app/services/authenticate.service';
 
 @Component({
   selector: 'app-cart',
@@ -16,7 +17,11 @@ export class CartComponent implements OnInit {
   cart: Training[] | undefined;
   amount: number = 0;
   dateOrder: Date = new Date();
-  constructor(public cartService: CartService, private router: Router) {}
+  constructor(
+    public cartService: CartService,
+    private router: Router,
+    private authService: AuthenticateService
+  ) {}
 
   /**
    * à l'initialisation du composant, récupération des données du panier via le service dédié
@@ -39,6 +44,10 @@ export class CartComponent implements OnInit {
    * Méthode de gestion de l'étape suivante de la commande en renvoyant vers le composant de gestion client (formulaire)
    */
   onNewOrder() {
-    this.router.navigateByUrl('customer');
+    if (this.authService.getConnection() === true) {
+      this.router.navigateByUrl('customer');
+    } else {
+      alert('Vous devez être connecté pour passer commande');
+    }
   }
 }
